eae452ca47db9eb73b82e2d8adeef389e4f319c2
Kunj1234567890 <97776880+Kunj1234567890@users.noreply.github.com>
2023-07-28 17:40:16 +0800

    Add files via upload

--- .j.js.js.js.j.js.js.js.j.js.js.js.j.js.js.js
+++ .j.js.js.js.j.js.js.js.j.js.js.js.j.js.js.js
@@ -1,8 +1,19 @@
-function setup() {
-    canvas = createCanvas(500,500);
-    canvas.position(560,150);
+Backrooms_soundstrack="";
+Megalovania="";
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
     video = createCapture(VIDEO);
-    video.size(500,500);
-    posenet = ml5.poseNet(video , modelloaded );
-    posenet.on('pose' , gotposes);
- }
+    video.hide();
+}
+
+function preload(){
+    Backrooms_soundstrack = loadSound("_Level !_ But It's Actually a Soundtrack REMASTERED.mp3");
+    Megalovania = loadSound("Notoriety - Brick Bank [LOUD OST].mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+}
