ffe3c7c2feba72456b6adfb2e90d0d679da41051
Kunj1234567890 <97776880+Kunj1234567890@users.noreply.github.com>
2023-08-04 17:43:38 +0800

    Add files via upload

--- .j.js.js.js.j.js.js.js.j.js.js.js.j.js.js.js
+++ .j.js.js.js.j.js.js.js.j.js.js.js.j.js.js.js
@@ -1,10 +1,13 @@
 Backrooms_soundstrack="";
 Megalovania="";
+rightWrist_x = 0;
+rightWrist_y = 0;
+leftWrist_x = 0;
+leftWrist_y = 0;
 
 function setup(){
-    canvas = createCanvas(600,530);
+    canvas = createCanvas(600,500);
     canvas.center();
-
     video = createCapture(VIDEO);
     video.hide();
 }
@@ -15,5 +18,22 @@ function preload(){
 }
 
 function draw(){
-    image(video,0,0,600,530);
+    image(video,0,0,600,500);
+}
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+         
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
+    }
 }
